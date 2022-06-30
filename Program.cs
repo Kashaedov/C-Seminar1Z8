135b0dfa98329c0b22dc0c488953f0f0bf9b4087
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-06-30 19:24:58 +0300

    zadacha8

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите  число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.Write ($" Все четные числа от 1 до {numberN}: ");
+while (count <= numberN)
+{
+   
+    Console.Write (count + ", ");
+    count = count + 2;
+}
